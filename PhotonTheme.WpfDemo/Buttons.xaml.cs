265bce14b9d07547812f5e28a27c76937f2f0479
Albert <aalonso@protonmail.com>
2019-07-24 18:55:28 +0200

    Small upgrade to buttons interactive demo

--- PhotonTheme.WpfDemo/Buttons.xaml.cs
+++ PhotonTheme.WpfDemo/Buttons.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace PhotonTheme.WpfDemo
 {
@@ -10,6 +12,71 @@ namespace PhotonTheme.WpfDemo
         public Buttons()
         {
             InitializeComponent();
+        }
+
+        private void ThemeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox?.SelectedItem == null) return;
+
+            switch (combobox.SelectedIndex)
+            {
+                case 1:
+                    ShowcaseButton.Foreground = Helper.Get<Brush>("DarkForegroundBrush");
+                    ShowcaseButton.Background = Helper.Get<Brush>("PrimaryBrush");
+                    break;
+                case 2:
+                    ShowcaseButton.Foreground = Helper.Get<Brush>("PrimaryBrush");
+                    ShowcaseButton.Background = Helper.Get<Brush>("LightBrush");
+                    break;
+                default:
+                    ShowcaseButton.Foreground = Helper.Get<Brush>("LightForegroundBrush");
+                    ShowcaseButton.Background = Helper.Get<Brush>("MediumBrush");
+                    break;
+            }
+        }
+
+        private void SizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox?.SelectedItem == null) return;
+
+            switch (combobox.SelectedIndex)
+            {
+                case 0:
+                    ShowcaseButton.FontSize = 16;
+                    break;
+                case 1:
+                    ShowcaseButton.FontSize = 14;
+                    break;
+                default:
+                    ShowcaseButton.FontSize = 11;
+                    break;
+            }
+        }
+
+        private void BackgroundComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox?.SelectedItem == null) return;
+            if (combobox.Items[combobox.SelectedIndex] is ComboBoxItem comboBoxItem)
+                ShowcaseButton.Background = Helper.Get<Brush>(comboBoxItem.Content.ToString());
+        }
+
+        private void ForegroundComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox?.SelectedItem == null) return;
+            if (combobox.Items[combobox.SelectedIndex] is ComboBoxItem comboBoxItem)
+                ShowcaseButton.Foreground = Helper.Get<Brush>(comboBoxItem.Content.ToString());
+        }
+    }
+
+    public static class Helper
+    {
+        public static T Get<T>(string resourceName) where T : class
+        {
+            return Application.Current.TryFindResource(resourceName) as T;
         }
     }
 }
