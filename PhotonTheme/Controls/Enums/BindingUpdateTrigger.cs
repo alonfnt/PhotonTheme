c76c6df103a2c0473326793e64cc7ccd4f345927
Albert <aalonso@protonmail.com>
2019-07-24 17:36:38 +0200

    Added Verification Trigger to VerificationBox
    Now the user can decide when to call the verification method. This helps for when the verification is down in a server.

---
+++ PhotonTheme/Controls/Enums/BindingUpdateTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotonTheme.Controls
+{
+    public enum BindingUpdateTrigger
+    {
+        OnPropertyChanged,
+        LostFocus
+    }
+}
